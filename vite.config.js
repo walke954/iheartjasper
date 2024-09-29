d1e33214bca161f819825913cffc0eb4c94c37cd
walke954 <walkjo53@gmail.com>
2024-09-29 12:49:58 -0700

    fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: '../docs',
-    emptyOutDir: true, // also necessary
-  }
+  plugins: [react()]
 })
