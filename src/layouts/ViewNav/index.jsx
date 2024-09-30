cae09f043b17fc2fed8a0484019cb6c7dc393954
walke954 <walkjo53@gmail.com>
2024-09-30 07:56:33 -0700

    moved whitespace outside header

--- src/layouts/ViewNav/index.jsx
+++ src/layouts/ViewNav/index.jsx
@@ -22,28 +22,30 @@ const ViewNav = (props, ref) => {
   }
 
   return (
-    <div className={css.container}>
+    <div>
       <div className={css.whitespace} />
-      <nav className={css.navTopbar}>
-        <div className={css.topbarActions}>
-          <button
-            className={css.menuBtn}
-            onClick={() => setCollapse(!collapse)}
-          >
-            <img src={menu} alt="show-menu-icon" />
-          </button>
-        </div>
-        <div className={dropdownStyles.join(' ')}>
+      <div className={css.container}>
+        <nav className={css.navTopbar}>
+          <div className={css.topbarActions}>
+            <button
+              className={css.menuBtn}
+              onClick={() => setCollapse(!collapse)}
+            >
+              <img src={menu} alt="show-menu-icon" />
+            </button>
+          </div>
+          <div className={dropdownStyles.join(' ')}>
+            {navigation}
+          </div>
+        </nav>
+        <nav className={css.navSidebar}>
           {navigation}
-        </div>
-      </nav>
-      <nav className={css.navSidebar}>
-        {navigation}
-      </nav>
-      <main className={css.contentContainer}>
-        <div className={overlayStyles} onClick={() => setCollapse(true)} />
-        {content}
-      </main>
+        </nav>
+        <main className={css.contentContainer}>
+          <div className={overlayStyles} onClick={() => setCollapse(true)} />
+          {content}
+        </main>
+      </div>
     </div>
   );
 }
